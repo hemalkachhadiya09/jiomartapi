e98ff400decbd6041f332dc4ea86f67a81b54c67
Hemalkachhadiya09 <kachhadiyahemal3@gmail.com>
2022-06-06 16:08:47 +0530

    code

---
+++ apidocument.md
@@ -0,0 +1,48 @@
+///Page-1
+
+>Search By items
+ http://localhost:6778/product/1
+
+>List of Category
+http://localhost:6778/category
+
+>Top Rated items
+ http://localhost:6778/filterRating/1?sort=-1
+
+
+///Page-2
+
+>List of Clothes on basis of Brand
+ http://localhost:6778/items/1?brandName=Nike
+
+> Filter on basis of Size
+  http://localhost:6778/items/1?size=XL
+
+>Filter on basis of size and Brand
+  http://localhost:6778/items/1?size=XL&brandName=Nike
+
+>Sort on basis of cost
+  >High to Low
+    http://localhost:6778/filter/1?sort=-1
+
+  >Low To High
+    http://localhost:6778/filter/1
+
+>Filter basis on the brand and price
+ http://localhost:6778/items/1?size=XL&%20sort=-1
+
+
+>Sort by Rating
+  http://localhost:6778/filterRating/1?sort=-1
+
+
+///Page-3
+
+>Details of Items
+  http://localhost:6778/items
+
+///Page -4
+
+>Details of placed orders
+ localhost:6778/placeOrder
+ 
---
+++ app.js
@@ -0,0 +1,184 @@
+let express=require('express');
+let app=express();
+let dotenv=require('dotenv').config();
+let bodyParser=require('body-parser');
+let cors = require('cors');
+let mongo=require('mongodb');
+let port=process.env.PORT || 8654;
+let mongoUrl=process.env.MongoLiveUrl;
+let MongoClient=mongo.MongoClient;
+let db;
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cors())
+
+app.get('/',(req,res)=>{
+    res.send('JioMart API running')
+})
+
+app.get('/category',(req,res)=>{
+
+    db.collection('category').find().toArray((err,result)=>{
+       if(err) throw err;
+       res.send(result)
+     })
+}) 
+
+app.get('/category/:categoryId',(req,res)=>{
+    let categoryId=Number(req.params.categoryId) ;
+    query={}
+    if(categoryId)
+  {
+    query={"Category_id":categoryId}
+  }
+ 
+
+    db.collection('category').find(query).toArray((err,result)=>{
+       if(err) throw err;
+       res.send(result)
+     })
+})
+
+app.get('/items',(req,res)=>{
+
+    db.collection('items').find().toArray((err,result)=>{
+       if(err) throw err;
+       res.send(result)
+     })
+})
+app.get('/product/:productId',(req,res)=>{
+    let productId=Number(req.params.productId) ;
+    query={}
+    if(productId)
+  {
+    query={"Product_id":productId}
+  }
+ 
+
+    db.collection('items').find(query).toArray((err,result)=>{
+       if(err) throw err;
+       res.send(result)
+     })
+}) 
+app.get('/items/:categoryId',(req,res)=>{
+    let sort={Price:1};
+    let categoryId=Number(req.params.categoryId);
+    let brandName=req.query.brandName;
+    let size=req.query.size;
+
+    query={}
+    if(req.query.sort)
+    {
+        sort={Price:req.query.sort}
+    }
+    if(brandName && size )
+    {
+       query= {"Category_id":categoryId,"Brand":brandName,"Size":size}  
+    }
+   else if(brandName)
+  {
+    query={"Category_id":categoryId,"Brand":brandName}
+  }
+  else if(size){
+    query={"Category_id":categoryId,"Size":size}
+  }
+  else {
+    query={"Category_id":categoryId}
+}
+  
+ 
+
+    db.collection('items').find(query).toArray((err,result)=>{
+       if(err) throw err;
+       res.send(result)
+     })
+})
+
+app.get('/filter/:categoryId',(req,res)=>{
+
+    let sort={Price:1};
+    let categoryId=Number(req.params.categoryId);
+    if(req.query.sort)
+    {
+        sort={Price:req.query.sort}
+    }
+    query={}
+    if(categoryId)
+  {
+    query={"Category_id":categoryId}
+  }
+ 
+
+
+    db.collection('items').find(query).sort(sort).toArray((err,result)=>{
+       if(err) throw err;
+       res.send(result)
+     })
+})
+app.get('/filterRating/:categoryId',(req,res)=>{
+
+    let sort={Rating:1};
+    let categoryId=Number(req.params.categoryId);
+    if(req.query.sort)
+    {
+        sort={Rating:req.query.sort}
+    }
+    query={}
+    if(categoryId)
+  {
+    query={"Category_id":categoryId}
+  }
+ 
+
+
+    db.collection('items').find(query).sort(sort).toArray((err,result)=>{
+       if(err) throw err;
+       res.send(result)
+     })
+})
+app.post('/placeOrder',(req,res) => {
+
+    db.collection('order').insert(req.body,(err,result) => {
+      if(err) throw err;
+      res.send(result)
+    })
+  })
+// app.get('/items/:brandName',(req,res)=>{
+     
+    
+//     let brandName=req.query.brandName;
+
+//     query={}
+//     if(brandName)
+//   {
+//     query={"Brand":brandName}
+//   }
+   
+ 
+
+//     db.collection('items').find(query).toArray((err,result)=>{
+//        if(err) throw err;
+//        res.send(result)
+//      })
+// })
+ 
+
+
+
+///connection with db
+MongoClient.connect(mongoUrl,(err,client) => {
+    if(err)  {console.log(`Error While Connecting`)};
+    
+    db = client.db('jiomartdb');
+    
+  
+    app.listen(port,(err) => {
+      if(err) throw err;
+      console.log(`Express Server listening on port ${port}`)
+    })
+  })
+
+  //MongoUrl="mongodb://localhost:27017"
+   
+   
+   
